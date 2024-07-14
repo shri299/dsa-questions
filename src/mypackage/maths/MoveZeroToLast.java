e9ad53d5886e99593d34b657e91e0d91d9ca68ee
Srishti Srivastava <ssrishti508@gmail.com>
2024-07-14 21:45:10 +0530

    move zeroes to the end

---
+++ src/mypackage/maths/MoveZeroToLast.java
@@ -0,0 +1,27 @@
+package mypackage.maths;
+
+public class MoveZeroToLast {
+
+    //move all the zeroes in ana array to last
+    public static void main(String[] args) {
+        int[] nums = {0,1};
+        moveZeroes(nums);
+    }
+
+    public static void moveZeroes(int[] nums) {
+        int index = 0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+            int end_count = nums.length - index + 1;
+            for(int t = 1;t<end_count;t++){
+                nums[index] = 0;
+                index++;
+            }
+
+    }
+}
