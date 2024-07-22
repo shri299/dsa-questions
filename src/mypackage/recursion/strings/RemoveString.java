c11cbaf43b38cbbe1390e17fd49169175db85b6d
Srishti Srivastava <ssrishti508@gmail.com>
2024-07-22 23:05:18 +0530

    remove string from string

--- src/mypackage/recursion/strings/RemoveString.java
+++ src/mypackage/recursion/strings/RemoveString.java
@@ -4,7 +4,9 @@ public class RemoveString {
 
     //remove string
     public static void main(String[] args) {
-        System.out.println(skipString("bcappledfg"));
+        //System.out.println(skipString("bcappledfg"));
+        System.out.println(skipStringOne("bcappopdfg"));
+        System.out.println(skipStringOne("bcappledfg"));
     }
 
     public static String skipString(String string){
@@ -15,6 +17,22 @@ public class RemoveString {
             return skipString(string.substring(5));
         }else {
             return string.charAt(0) + skipString(string.substring(1));
+        }
+    }
+
+    //only skip "app", when it is not equal to apple
+
+    public static String skipStringOne(String string){
+        if (string.isEmpty()){
+            return "";
+        }
+        if (string.startsWith("app")){
+            if (!string.startsWith("apple")){
+                return skipStringOne(string.substring(3));
+            }
+            return string.charAt(0) + skipStringOne(string.substring(1));
+        }else {
+            return string.charAt(0) + skipStringOne(string.substring(1));
         }
     }
 }
