c846c87859a26269f24821643770bafd30115c2d
Srishti Srivastava <ssrishti508@gmail.com>
2024-07-28 22:43:34 +0530

    print path of maze diagonal

--- src/mypackage/recursion/backtracking/CountPathOfMaze.java
+++ src/mypackage/recursion/backtracking/CountPathOfMaze.java
@@ -4,7 +4,8 @@ public class CountPathOfMaze {
 
     public static void main(String[] args) {
         //System.out.println(count(3,3));
-        printPath(3,3,"");
+        //printPath(3,3,"");
+        printPathDiagonal(3,3,"");
     }
 
     //count number of ways to reach end
@@ -34,6 +35,27 @@ public class CountPathOfMaze {
         }
         if (c>1){
             printPath(r,c-1,output+"r");
+        }
+
+    }
+
+    //we can also go diagonally
+
+    static void printPathDiagonal(int r, int c, String output){
+
+        if (r==1 && c==1){
+            System.out.println(output);
+            return;
+        }
+
+        if (r>1){
+            printPathDiagonal(r-1,c,output+"d");
+        }
+        if (c>1){
+            printPathDiagonal(r,c-1,output+"r");
+        }
+        if (r>1 && c>1){
+            printPathDiagonal(r-1,c-1,output+"x");
         }
 
     }
