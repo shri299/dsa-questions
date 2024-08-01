b373e357544a6de844129ca15cbf704bd14642d3
Srishti Srivastava <ssrishti508@gmail.com>
2024-08-01 23:26:09 +0530

    insert last and in between

--- src/mypackage/linkedlists/LL.java
+++ src/mypackage/linkedlists/LL.java
@@ -21,6 +21,44 @@ public class LL {
         size = size+1;
     }
 
+    public void insertLast(int val){
+        Node node = new Node(val);
+        if (head==null){
+            head=node;
+            tail=head;
+        }else {
+            Node temp = head;
+            while (temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next=node;
+            tail=node;
+        }
+        size=size+1;
+    }
+
+    public void insertInBetween(int index, int val){
+        if (index==1){
+            insertFirst(val);
+        }else if (index==size){
+            insertLast(val);
+        }else {
+            Node node = new Node(val);
+            Node temp = head;
+            int counter = 1;
+            while (temp!=null){
+                counter++;
+                if (counter==index){
+                    Node temp1 = temp.next;
+                    temp.next=node;
+                    node.next=temp1;
+                }
+                temp=temp.next;
+            }
+            size=size+1;
+        }
+    }
+
     public void display(){
         Node temp = head;
         while (temp!=null){
