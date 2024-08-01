3ba9d4b3796b1d650f88d7f990dd08c63c3d475b
Srishti Srivastava <ssrishti508@gmail.com>
2024-08-01 22:15:34 +0530

    insert first and display

---
+++ src/mypackage/linkedlists/LL.java
@@ -0,0 +1,46 @@
+package mypackage.linkedlists;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL (){
+        this.size=0;
+    }
+
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next=head;
+        head = node;
+
+        if (tail==null){
+            tail=head;
+        }
+        size = size+1;
+    }
+
+    public void display(){
+        Node temp = head;
+        while (temp!=null){
+            System.out.print(temp.val+"->");
+            temp = temp.next;
+        }
+        System.out.print("end");
+    }
+
+    private class Node {
+        private int val;
+        private Node next;
+
+        public Node (int val){
+            this.val=val;
+        }
+
+        public Node (int val, Node next){
+            this.val=val;
+            this.next=next;
+        }
+    }
+}
