ec5a400a46d9d48db3e2db113bd6f417926fd71a
Srishti Srivastava <ssrishti508@gmail.com>
2024-08-25 21:50:30 +0530

    binary search tree implementation

---
+++ src/mypackage/binarysearchtree/Implementation.java
@@ -0,0 +1,72 @@
+package mypackage.binarysearchtree;
+
+public class Implementation {
+
+    Node root;
+
+    public Implementation(){
+
+    }
+
+    class Node{
+        int val;
+        Node left;
+        Node right;
+        int height;
+
+        public Node (int val){
+            this.val=val;
+        }
+        public int getValue(){
+            return val;
+        }
+    }
+
+    public int height(Node node){
+        return node==null ? -1 : node.height;
+    }
+
+    public boolean isEmpty(){
+        return root==null;
+    }
+
+    public int getHeight(Node node){
+        if (root==null)
+            return -1;
+        return node.height;
+    }
+
+    public Node insert(int val, Node node){
+        if (node==null){
+            return new Node(val);
+        }
+        if (val<node.val){
+            node.left=insert(val,node.left);
+        }
+        if (val>node.val){
+            node.right=insert(val,node.right);
+        }
+
+        node.height=Math.max(height(node.left),height(node.right))+1;
+
+        return node;
+
+    }
+
+    public void display(){
+        display(root,"");
+    }
+
+    private void display(Node root, String s) {
+        if (root==null){
+            return;
+        }
+        System.out.println(s+root.val);
+
+        Node left = root.left;
+        display(left,s+"----");
+
+        Node right = root.right;
+        display(right,s+"----");
+    }
+}
