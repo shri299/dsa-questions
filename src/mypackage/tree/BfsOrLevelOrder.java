9aaa533bb691b3d1183bd8d4d9266ffd85ce3120
Srishti Srivastava <ssrishti508@gmail.com>
2024-08-31 21:38:18 +0530

    level order successor

--- src/mypackage/tree/BfsOrLevelOrder.java
+++ src/mypackage/tree/BfsOrLevelOrder.java
@@ -130,4 +130,7 @@ public class BfsOrLevelOrder {
     }
 
 
+
+
+
 }
--- src/mypackage/tree/LevelOrderSuccessor.java
+++ src/mypackage/tree/LevelOrderSuccessor.java
@@ -1,99 +1,134 @@
 package mypackage.tree;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class LevelOrderSuccessor {
 
     Node root;
 
-    public LevelOrderSuccessor (){
+    public LevelOrderSuccessor() {
 
     }
 
-    class Node{
+    class Node {
         int val;
         Node left;
         Node right;
 
-        public Node (int val){
-            this.val=val;
+        public Node(int val) {
+            this.val = val;
         }
     }
 
-    public void insert(Scanner scanner){
+    public void insert(Scanner scanner) {
         System.out.println("Enter the root node");
         int val = scanner.nextInt();
         root = new Node(val);
-        insertHelper(root,scanner);
+        insertHelper(root, scanner);
     }
 
     private void insertHelper(Node root, Scanner scanner) {
-        System.out.println("do you wish to enter the left of: "+root.val);
+        System.out.println("do you wish to enter the left of: " + root.val);
         boolean left = scanner.nextBoolean();
-        if (left){
-            System.out.println("enter the left of: "+root.val);
+        if (left) {
+            System.out.println("enter the left of: " + root.val);
             int leftVal = scanner.nextInt();
-            root.left=new Node(leftVal);
-            insertHelper(root.left,scanner);
+            root.left = new Node(leftVal);
+            insertHelper(root.left, scanner);
         }
 
-        System.out.println("do you wish to enter the right of: "+root.val);
+        System.out.println("do you wish to enter the right of: " + root.val);
         boolean right = scanner.nextBoolean();
-        if (right){
-            System.out.println("enter the right of: "+root.val);
+        if (right) {
+            System.out.println("enter the right of: " + root.val);
             int rightVal = scanner.nextInt();
-            root.right=new Node(rightVal);
-            insertHelper(root.right,scanner);
+            root.right = new Node(rightVal);
+            insertHelper(root.right, scanner);
         }
     }
 
 
-    public void display(){
-        displayHelper(root,"");
+    public void display() {
+        displayHelper(root, "");
     }
 
     private void displayHelper(Node root, String s) {
-        if (root==null){
+        if (root == null) {
             return;
         }
-        System.out.println(s+root.val);
-        displayHelper(root.left,s+"----");
-        displayHelper(root.right,s+"----");
+        System.out.println(s + root.val);
+        displayHelper(root.left, s + "----");
+        displayHelper(root.right, s + "----");
     }
 
     //level order successor of a node let's say 4
 
-    public void calculate(Node root, int key){
+    public void calculate(Node root, int key) {
         Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
 
-        while (!queue.isEmpty()){
-            int levelSize = queue.size();
-
-            for (int i=0; i< levelSize; i++){
-                Node target = queue.poll();
-
-                if (target.left!=null){
-                    queue.offer(target.left);
+        while (!queue.isEmpty()) {
+            Node target = queue.poll();
+            if (target.left != null) {
+                queue.offer(target.left);
+            }
+            if (target.right != null) {
+                queue.offer(target.right);
+            }
+            //check of value
+            if (target.val == key) {
+                Node answer = queue.poll();
+                if (answer == null) {
+                    System.out.println("no next node");
+                } else {
+                    System.out.println(answer.val);
                 }
-
-                if (target.right!=null){
-                    queue.offer(target.right);
-                }
-                //check of value
-                if (target.val==key){
-                    Node answer = queue.poll();
-                    if (answer==null){
-                        System.out.println("no next node");
-                    }else {
-                        System.out.println(answer.val);
-                    }
-                    break;
-                }
+                break;
             }
         }
+    }
+
+
+    //zig zag traversal
+
+    public List<List<Integer>> traversal(Node root){
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Node> deque = new ArrayDeque<>();
+        int flag=0;
+        if (root==null){
+            return result;
+        }
+        deque.addFirst(root);
+        while (!deque.isEmpty()){
+            int levelSize = deque.size(); //initially its 1
+            List<Integer> subList = new ArrayList<>();
+            for (int i=0;i<levelSize;i++){
+                //we have two cases either remove from front or from back
+                if (flag % 2 == 0){
+                    Node target = deque.pollFirst(); //remove first and add to subList
+                    subList.add(target.val);
+                    if (target.left!=null){
+                        deque.addLast(target.left);
+                    }
+                    if (target.right!=null){
+                        deque.addLast(target.right);
+                    }
+                }else {
+                    //remove from back, insert children in the front
+                    Node target = deque.pollLast();
+                    subList.add(target.val);
+                    if (target.right!=null){
+                        deque.addFirst(target.right);
+                    }
+                    if (target.left!=null){
+                        deque.addFirst(target.left);
+                    }
+                }
+            }
+            flag = flag+1;
+            result.add(subList);
+        }
+        return result;
     }
 
 
