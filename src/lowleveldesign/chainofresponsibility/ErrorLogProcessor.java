1c89e2325bf04f10945c73caf9f2f39693335228
Srishti Srivastava <ssrishti508@gmail.com>
2024-10-03 14:07:47 +0530

    chain of responsibility pattern

---
+++ src/lowleveldesign/chainofresponsibility/ErrorLogProcessor.java
@@ -0,0 +1,16 @@
+package lowleveldesign.chainofresponsibility;
+
+public class ErrorLogProcessor extends LogProcessor{
+
+    ErrorLogProcessor (LogProcessor nextLogProcessor){
+        super(nextLogProcessor);
+    }
+
+    public void log(int logType, String message){
+        if (logType==ERROR){
+            System.out.println("ERROR "+message);
+        }else {
+            super.log(logType,message);
+        }
+    }
+}
