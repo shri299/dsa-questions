f78281456c36af5996a8316919546e539592ffb2
Srishti Srivastava <ssrishti508@gmail.com>
2025-02-21 22:59:07 +0530

    sum of subsets oof array

---
+++ src/dsa/recursion/SumOfSubsets.java
@@ -0,0 +1,24 @@
+package dsa.recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumOfSubsets {
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,1};
+        List<Integer> ans = new ArrayList<>();
+        helper(0,arr,ans,0);
+        System.out.println(ans);
+    }
+
+    private static void helper(int index, int[] arr, List<Integer> ans,int sum) {
+        if(index>=arr.length){
+            ans.add(sum);
+            return;
+        }
+
+        helper(index+1,arr,ans,sum+arr[index]);
+        helper(index+1,arr,ans,sum);
+    }
+}
